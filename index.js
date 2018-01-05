4a160606bf62348d12da52d4570e407b9ab6eede
Brian David Lehto <bdlden1978@outlook.com>
2018-01-05 21:53:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ kittens = ['Milo', 'Otis', 'Garfield']
 
 function destructivelyAppendKitten(name) {
   return kittens.push(name)
+}
 
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name)
 }
